706719f2c7a1aa93765fb4dd4cbb7784dbe3ee61
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-12 21:08:23 +0700

    Starter-backend_API

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+// initialize App express
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const port = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("MongoDb connected!");
+    server.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
+}).catch((error) => {
+    console.log({ error });
+    process.exit(1);
+})
