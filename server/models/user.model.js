b620116e3e828e2ad7b8ca579f40bc547b8a7d44
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-14 14:05:56 +0700

    update-UserModels

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -23,6 +23,31 @@ const UserSchema = new mongoose.Schema({
         select: false,
     }
 }, model);
+
 UserSchema.methods.setPassword = (password) => {
-    
+    this.salt = crypto.randomBytes(16).toString("hex")
+
+    this.password = crypto.pbkdf2Sync(
+        password,
+        this.salt,
+        1000,
+        64,
+        "sha512"
+    ).toString("hex");
 }
+
+UserSchema.methods.validPassword = (password) => {
+    const hash = crypto.pbkdf2Sync(
+        password,
+        this.salt,
+        1000,
+        64,
+        "sha512",
+    ).toString("hex");
+
+    return this.password === hash;
+}
+
+const userModel = mongoose.model("User", UserSchema);
+
+export default userModel;
