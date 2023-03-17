82dac2db499de81da7082eb4f86ea8252fab9950
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-17 22:36:40 +0700

    update-SignIn-User-Controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import userModel from "../models/user.model.js";
-import  jsonWebToken from "jsonwebtoken";
+import  jsonwebToken from "jsonwebtoken";
 import responseHandler from "../handlers/response.handler.js";
 
 const signUp = async (req, res) => {
@@ -13,14 +13,14 @@ const signUp = async (req, res) => {
         if(checkUser) return responseHandler.badRequest(res, "Username has been already used!");
 
         const user = new userModel();
-        
+
         user.displayName = displayName;
         user.username = username;
         user.setPassword(password);
         // Save
         await user.save();
         // generate Token
-        const token = jsonWebToken.sign(
+        const token = jsonwebToken.sign(
             { data: user.id },
             process.env.JWT_SECRET_KEY,
             { expiresIn: "24h" }
@@ -37,3 +37,36 @@ const signUp = async (req, res) => {
         console.log(error);
     };
 };
+
+const signIn  = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne({ username }).select("Username password salt id displayName");
+
+        if(!user)
+            return responseHandler(res, "User not found!");
+
+        if(!user.validPassword(password))
+            return responseHandler.badRequest(res, "Wrong Password");
+
+        const token = jsonwebtoken.sign(
+            { data: user.id },
+            process.env.TOKEN_SECRET,
+            { expiresIn: "24h" },
+        );
+
+        user.password = undefined;
+        user.salt = undefined;
+
+        responseHandler.created(res, {
+            token,
+            ...user._doc,
+            id: user.id,
+        });
+    } catch (error) {
+        responseHandler.error(res);
+        console.log(error);
+    }
+};
+
