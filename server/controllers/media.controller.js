a78c7f8bed1a4fd646d3281dd8095172ece07d27
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-19 17:44:22 +0700

    update-Media-Controller@2

--- server/controllers/media.controller.js
+++ server/controllers/media.controller.js
@@ -2,7 +2,9 @@ import tmdbApi from "../tmdb/tmdb.api.js";
 import responseHandler from "../handlers/response.handler.js";
 import userModel from "../models/user.model.js";
 import favoriteModel from "../models/favorite.model.js";
-import reviewModel from "../models/review.model.js";
+import reviewModel from "../models/review.model.js"
+import tokenMiddlerware from "../middlewares/token.middleware.js";;
+
 
 const getList = async (req, res) => {
     try {
@@ -52,3 +54,39 @@ const search = async (req, res) => {
         console.log(error);
     }
 };
+
+const getDetail = async (req, res) => {
+    try {
+        const { mediaType, mediaId } = req.params;
+        const params = { mediaType, mediaId };
+
+        const media = await tmdbApi.mediaDetail(params);
+        media.credits = await tmdbApi.mediaCredits(params);
+
+        const videos = await tmdbApi.mediaVideos(params);
+        media.videos = videos;
+
+        const recommend = await tmdbApi.mediaRecommend(params);
+        media.recommend = recommend.results;
+        media.images = await tmdbApi.mediaImages(params);
+
+        const tokenDecoded = tokenMiddlerware(req);
+
+        if(tokenDecoded) {
+            const user = await userModel.findById(tokenDecoded.data);
+
+            if(user) {
+                const isFavorite = await favoriteModel.findOne({
+                    user: user.id, mediaId });
+                media.isFavorite = isFavorite !== null;
+            }
+        }
+        media.reviews = await reviewModel.find({ mediaId }).populate("user").sort("-createAt");
+        responseHandler.ok(res, media);
+    } catch (error) {
+        responseHandler.error(res);
+        console.log(error);
+    }
+};
+
+export default { getList, getGenres, search, getDetail };
