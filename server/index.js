824a5cf947fae31a1bcfabcb255580385aae45c3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-09 09:55:52 +0700

    update-limit-req

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
+import { rateLimit } from "express-rate-limit";
 
 import routes from "./routes/index.js";
 
@@ -16,6 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/api/v1", routes);
 
+app.get('/api/v1', async (req, res) => {
+    res.status(200).json({
+        message: 'Hello from movies database api v1',
+    });
+})
+
+const limiter = rateLimit({
+    windowMs: 600 * 600 * 1000,
+    max: 500,
+    message: "Too many requests from this IP, please try again in an hour",
+});
+
+app.use(limiter);
+
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
@@ -23,7 +38,7 @@ const server = http.createServer(app);
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("MongoDb connected!");
     server.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
+        console.log(`Server is listening on port http://localhost:${port}`);
     });
 }).catch((error) => {
     console.log({ error });
