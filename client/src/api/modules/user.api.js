20cd66ef6fe149b2ed8a5efd7b193123e76974b3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-10 21:23:14 +0700

    update-tmdb-config-js

---
+++ client/src/api/modules/user.api.js
@@ -0,0 +1,11 @@
+import privateClient from "../client/private.client";
+import publicClient from "../client/public.client";
+
+const userEndpoints = {
+    signin: "user/signin",
+    signup: "user/signup",
+    getInfo: "user/info",
+    passwordUpdate: "user/update-password",
+};
+
+
