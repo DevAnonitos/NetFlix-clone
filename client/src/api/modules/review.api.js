d5fba2b4f926e68b66ee51df66d418810badbf09
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-11 10:03:53 +0700

    update-review-api-client

---
+++ client/src/api/modules/review.api.js
@@ -0,0 +1,57 @@
+import privateClient from "../client/public.client.js";
+
+const reviewEndpoints = {
+    list: "review",
+    add: "review",
+    remove: ({ reviewId }) => `reviews/${reviewId}`,
+};
+
+const reviewApi = {
+    add: async ({
+        mediaId,
+        mediaType,
+        mediaTitle,
+        mediaPoster,
+        content,
+    }) => {
+        try {
+            const response = await privateClient.post(
+                reviewEndpoints.add,
+                {
+                    mediaId,
+                    mediaPoster,
+                    mediaType,
+                    mediaTitle,
+                    content,
+                }
+            );
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    remove: async ({ reviewId }) => {
+        try {
+            const response = await privateClient.delete(reviewEndpoints.remove({
+                reviewId
+            }));
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    getList: async () => {
+        try {
+            const response = await privateClient.get(reviewEndpoints.list);
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+};
