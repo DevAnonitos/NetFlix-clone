77df0f230b1fe6dd29a7d03448b2288fe918faec
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-11 09:23:10 +0700

    update-User-api-client

--- client/src/api/modules/user.api.js
+++ client/src/api/modules/user.api.js
@@ -8,4 +8,59 @@ const userEndpoints = {
     passwordUpdate: "user/update-password",
 };
 
+const userApi = {
+    signin: async ({ username, password }) => {
+        try {
+            console.log("SendRequest");
+            const response = await publicClient.post(
+                userEndpoints.signin,
+                { username, password },
+            );
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    signup: async ({ username, password, confirmPassword, displayName }) => {
+        try {
+            const response = await publicClient.post(
+                userEndpoints.signup,
+                { username, password, confirmPassword, displayName },
+            );
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    getInfo: async () => {
+        try {
+            const  response = await privateClient.get(userEndpoints.getInfo);
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    passwordUpdate: async ({ password, newPassword, confirmNewPassword }) => {
+        try {
+            const response = await privateClient.put(
+                userEndpoints.signup,
+                { password, newPassword, confirmNewPassword },
+            );
+
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+};
+
+export default userApi;
+
 
