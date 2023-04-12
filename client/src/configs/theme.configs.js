02d93104852a0505f63fcab5acc9c9712fa60cc1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-12 21:19:10 +0700

    update-theme-config

--- client/src/configs/theme.configs.js
+++ client/src/configs/theme.configs.js
@@ -1,2 +1,50 @@
 import { createTheme } from "@mui/material/styles";
 import { colors } from "@mui/material";
+
+export const themeMode = {
+    dark: "dark",
+    light: "light",
+};
+
+const themeConfigs = {
+    custom: ({ mode }) => {
+        const customPalette = mode === themeMode.dark ? {
+            primary: {
+                main: "#20232a",
+                contrastText: "#ffffff",
+            },
+            secondary: {
+                main: "#61dafb",
+                contrastText: "#ffffff",
+            },
+            background: {
+                default: "#282c34",
+                paper: "#131313"
+            }
+        } : {
+            primary: {
+                main: " #61dafb"
+            },
+            secondary: {
+                main: "#17a2b8"
+            },
+            background: {
+                default: colors.grey["100"],
+            },
+        };
+
+        return createTheme({
+            palette: {
+                mode,
+                ...customPalette,
+            },
+            components: {
+                MuiButton: {
+                    defaultProps: {disableElevation: true},
+                },
+            },
+        })
+    },
+};
+
+export default themeConfigs;
