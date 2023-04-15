f940702d270c4361faa0e84ea074582b6ac9083c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-15 21:59:34 +0700

    update-App-config

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
 import React, { Suspense, lazy } from 'react';
-import { ThemeProvider } from '@mui/material/styles';
+import { useSelector } from "react-redux";
+import themeConfigs from './configs/theme.configs';
 import  CssBaseline  from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const App = () => {
+
+  const { themeMode } = useSelector((state) => state.themeMode);
+
   return (
     <>
-      App
+      <ThemeProvider
+        theme={themeConfigs.custom({ mode: themeMode })}
+      >
+        {/*--------------------Config Toast React-------------------- */}
+        <ToastContainer
+          position='bottom-left'
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnFocusLoss
+          pauseOnHover
+          theme={themeMode}
+        />
+        {/* --------------------MUI CSS baseline--------------------- */}
+        <CssBaseline />
+      </ThemeProvider>
     </>
   );
 };
