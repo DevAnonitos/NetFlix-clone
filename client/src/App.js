55471e7da36f2a4e0a08bab3a5c31c66ba2f85ea
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-15 11:28:35 +0700

    update-Config-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
 import React, { Suspense, lazy } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import  CssBaseline  from '@mui/material/CssBaseline';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import 'react-toastify/dist/ReactToastify.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const App = () => {
   return (
