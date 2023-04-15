3eeea2b1d1d1069166b8705f2fd8a7155012dbb7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-15 21:11:55 +0700

    update-fix-error-features

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
 import themeModeSlice from "./features/themeModeSlice";
-import authModalSlice from "../configs/authModalSlice";
-import appStateSlice from "../configs/appStateSlice";
+import authModalSlice from "./features/authModalSlice";
+import appStateSlice from "./features/appStateSlice";
 import globalLoadingSlice  from "./features/globalLoadingSlice";
 
 const store = configureStore({
