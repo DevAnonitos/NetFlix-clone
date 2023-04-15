2a2417afec6d9595dd2ca410abb4d3eedf2898d0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-15 21:01:19 +0700

    update-store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
+import themeModeSlice from "./features/themeModeSlice";
+import authModalSlice from "../configs/authModalSlice";
+import appStateSlice from "../configs/appStateSlice";
+import globalLoadingSlice  from "./features/globalLoadingSlice";
 
 const store = configureStore({
     reducer: {
         user: userSlice,
+        themeMode: themeModeSlice,
+        authModal: authModalSlice,
+        appState: appStateSlice,
+        globalLoading: globalLoadingSlice,
     }
 });
 
