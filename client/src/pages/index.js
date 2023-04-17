a59a240ea02ae7bada994272da0227bb3358fe9d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-17 11:00:47 +0700

    update-Pages/indexJs

---
+++ client/src/pages/index.js
@@ -0,0 +1,19 @@
+import FavoriteList from "./FavoriteList";
+import HomePage from "./HomePage";
+import MediaDetail from "./MediaDetail";
+import MediaList from "./MediaList";
+import MediaSearch from "./MediaSearch";
+import PasswordUpdate from "./PasswordUpdate";
+import PersonDetail from "./PersonDetail";
+import ReviewList from "./ReviewList";
+
+export {
+    FavoriteList,
+    HomePage,
+    MediaDetail,
+    MediaList,
+    MediaSearch,
+    PasswordUpdate,
+    PersonDetail,
+    ReviewList
+}
