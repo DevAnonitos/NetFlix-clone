008d6800f275c4ac5dd0ed26f92c7a098e578649
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-17 16:26:53 +0700

    update-Add-ProtectedPage

--- client/src/components/common/ProtectedPage.jsx
+++ client/src/components/common/ProtectedPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
 
-const ProtectedPage = () => {
+const ProtectedPage = ({ children }) => {
+
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.user);
+
+    useEffect(() => {
+        dispatch(setAuthModalOpen(!user));
+    }, [user, dispatch]);
+
     return (
-        <div>ProtectedPage</div>
+        <>
+            {user ? children : null}
+        </>
     );
 };
 
