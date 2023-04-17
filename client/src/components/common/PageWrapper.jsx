b759a33f9daf2afbf638ef0db62dd9034069aa02
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-17 10:50:43 +0700

    update-PageWrapper

--- client/src/components/common/PageWrapper.jsx
+++ client/src/components/common/PageWrapper.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from "react-redux";
+import { setAppState } from "../../redux/features/appStateSlice";
 
-const PageWrapper = () => {
+const PageWrapper = ({ state, children }) => {
+
+    const dispatch =  useDispatch();
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+    }, []);
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+        dispatch(
+            setAppState(state)
+        );
+    }, [state, dispatch]);
+
+
     return (
-        <div>PageWrapper</div>
+        <>
+            {children}
+        </>
     );
 };
 
