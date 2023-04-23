c015c257f7e0f15d772e3ac898f6e82fc832becf
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-23 20:32:23 +0700

    update-FixError-Axios

--- server/axios/axios.client.js
+++ server/axios/axios.client.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
 const get = async (url) => {
-    const response = await axios.get(url);
-
-    return response.data;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.status === 400) {
+            console.log("Request failed with status code 400:", error.response.data);
+        } else {
+            console.log("An error occurred:", error.message);
+        }
+    }
 };
 
 export default { get };
